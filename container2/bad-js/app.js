70c010f690f8722ad47735c728be4fac81c6da15
Platy <daryl.limkz@gmail.com>
2022-09-12 10:18:50 +0800

    feat(3): made challenge a little harder
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- container2/bad-js/app.js
+++ container2/bad-js/app.js
@@ -1,34 +1,73 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 const app = express();
 
 const PORT = 8080;
 
-const WIN = `File: /root/.ssh/authorized_keys
-# If you're trying to find the private keys on the system, don't bother, it's not here. Or is it...? It's not.
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAH6kT9M1xXl6QSVH2TqGH7dzH3Mi1RwNl7c7axZdTuOARGWBM1Zbo6RfpxV9CUdvrFD5cz1rJUM0HIuqX+uNL4cmnuXJf/B7bfrtetBuAHd35oSlpQrgI55aHPcU0yqC+rM6etji6B2vX2ZjnIqqjsUEzk+Nf0JOnJ4T5MjusPTy8n+C41GxbzhCrahYrIAw4uvjCZSdyerI2QxCRvOJHp8ubJBIqrxtsGJS3bIPbq+bfNklYblkekVzhFVPysvekDJKKzo1s81liRxfjxwMvT6tu9lj7mLr9GVBxu0pd7Yb+s/Je3WuD0eOnt3nGHvwZ/aCACXkVlp+W7zLiFMI3I0= someuser@somecomputer
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCDQWAdr0k/BmPtCbGfA95X4vwXGYc4LjxProZfyxwTWReHf8DT1SgF74zeDN2i50D8xlFBitX9XeIOknFZINgZOKy/EpErRWJghYnx9gJ6lO0OSIBDgoix/D+g/1f88JvZ93z6+HbEwYvm7opNgPM6yxEjMrtazby3y+yrE7UZJLWwxEm924LtBhq/ngWHVUtiHyUgvrha4d+/DJDhOqJH4wlYldTp0nNMPB9/u/rie2IzzxAf5mK8h5f0kaia0ZWLUdHCanYhBXJQ7wDGwNyn4DNfzxW/Vv1KWgLKnvK1Ld1hXtH5F4C3GM2OGDKOJucmrEKMStbeHtjhGnAcpNcj someotheruser@somecomputer
-
-Congratulations on solving part 3/6! Here's your flag!
-FLAG{0bv10u5ly_d0_n07_u53_cl13n7_51d3_v4l1d4710n}`;
+const unlock = (password) => {
+    return (
+        password.length === 4 &&
+        8 * Math.pow(password.charCodeAt(0), 2) +
+            2 * Math.pow(password.charCodeAt(1), 4) -
+            3 * Math.pow(password.charCodeAt(2), 3) +
+            Math.pow(password.charCodeAt(3), 2) ===
+            3849056 &&
+        /^[F-Ht-v1-3@&*]+$/g.test(password)
+    );
+};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
     res.sendFile("index.html", { root: __dirname });
+});
+
+app.get("/robots.txt", (_, res) => {
+    res.write("Disallow: /app");
+    res.end();
+});
+
+app.get("/app", (_, res) => {
+    const app = fs.readFileSync("./app.js");
+    res.write(app);
+    res.end();
 });
 
 app.post("/login", async (req, res) => {
     const password = req.body.password;
+    const path = req.body.path;
 
-    await new Promise((resolve) => setTimeout(resolve, 500)); // Prevent bruteforce
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Prevent bruteforce
 
-    if (password === "G&2t") {
-        res.write(WIN);
-    } else {
-        res.status(301).redirect("/");
+    try {
+        if (!unlock(password)) {
+            res.status(301).redirect("/");
+            res.end();
+            return;
+        }
+
+        if (path === undefined) {
+            res.write("Successfully logged in!");
+            res.end();
+            return;
+        }
+
+        if (!fs.existsSync(path)) {
+            res.status(400).write("Path does not exist!");
+            res.end();
+            return;
+        }
+
+        if (fs.statSync(path).isDirectory()) res.write(JSON.stringify(fs.readdirSync(path)));
+        else res.write(fs.readFileSync(path));
+
+        res.end();
+    } catch (err) {
+        console.log(err);
+        res.write("Something went wrong");
+        res.end();
     }
-    res.end();
 });
 
 app.listen(PORT, () => {
