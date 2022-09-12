89d7cbcdf2e3968e68f2f1e6d83b3a25c5999cce
Platy <daryl.limkz@gmail.com>
2022-09-12 17:50:10 +0800

    build: changed ports from 8080 to 3000
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- container2/bad-js/app.js
+++ container2/bad-js/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const app = express();
 
-const PORT = 8080;
+const PORT = 3000;
 
 const unlock = (password) => {
     return (
