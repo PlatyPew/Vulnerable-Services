c45e5a6d69e05ec84c7521ae57b761e406e57829
Platy <daryl.limkz@gmail.com>
2022-10-01 09:14:50 +0800

    fix(1): leak in database creds
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- container1/apache/src/config.php
+++ container1/apache/src/config.php
@@ -1,6 +1,10 @@
 <?php
-    define('DB_SERVER', 'container1-db');
-    define('DB_USERNAME', 'sqldev');
-    define('DB_PASSWORD', 'Php=p@in');
-    define('DB_DATABASE', '2204_for_fun');
-    $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    try {
+        define('DB_SERVER', 'container1-db');
+        define('DB_USERNAME', 'sqldev');
+        define('DB_PASSWORD', 'Php=p@in');
+        define('DB_DATABASE', '2204_for_fun');
+        $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    } catch (Exception $e) {
+        echo "Database not initialised yet!";
+    }
